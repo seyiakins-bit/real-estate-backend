e21737b2da427a1517cd04bc3879e8c0dca6ce6f
seyiakins <seyibass10@gmail.com>
2025-11-18 12:58:35 +0100

    Add reset-db script and fix package.json

--- prisma/resetDb.js
+++ prisma/resetDb.js
@@ -5,11 +5,17 @@ const prisma = new PrismaClient();
 async function resetDatabase() {
   try {
     console.log("Resetting database...");
+
+    // Delete all migration records
+    await prisma.$executeRawUnsafe(`DELETE FROM "_prisma_migrations";`);
+
     // Drop the public schema (deletes all tables and enums)
     await prisma.$executeRawUnsafe(`DROP SCHEMA public CASCADE`);
+
     // Recreate the schema
     await prisma.$executeRawUnsafe(`CREATE SCHEMA public`);
-    console.log("Database schema reset complete!");
+
+    console.log("Database reset complete!");
   } catch (error) {
     console.error("Error resetting database:", error);
   } finally {
